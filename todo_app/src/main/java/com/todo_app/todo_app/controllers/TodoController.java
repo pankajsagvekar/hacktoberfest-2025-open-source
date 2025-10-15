cab23a2045db3da345c85b8ed6c632a740cf3992
bimbims125 <pramvendread@gmail.com>
2025-10-15 13:42:00 +0700

    feat: Get todos by id

--- todo_app/src/main/java/com/todo_app/todo_app/controllers/TodoController.java
+++ todo_app/src/main/java/com/todo_app/todo_app/controllers/TodoController.java
@@ -2,16 +2,19 @@ package com.todo_app.todo_app.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/todos")
 public class TodoController {
 
   // Get All Todos
-  @GetMapping("/todos")
+  @GetMapping()
   public ResponseEntity<Map<String, Object>> getAll()
   {
     List<Map<String, Object>> todos = generateTodos(20);
@@ -19,6 +22,13 @@ public class TodoController {
     response.put("todos", todos);
     return ResponseEntity.ok(response);
   }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Map<String, Object>> getTodoById(@PathVariable Long id) {
+    Map<String, Object> todo = createTodo(id);
+    return ResponseEntity.ok(todo);
+  }
+
 
   private List<Map<String, Object>> generateTodos(int count) {
     List<Map<String, Object>> todos = new ArrayList<>();

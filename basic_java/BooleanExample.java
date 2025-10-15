81a6a8bc5b0ffa3e21615c8ef6f280168dd7dc2a
bimbims125 <pramvendread@gmail.com>
2025-10-15 09:05:20 +0700

    add: boolean basic java

---
+++ basic_java/BooleanExample.java
@@ -0,0 +1,10 @@
+package basic_java;
+
+public class BooleanExample {
+  public static void main(String[] args) {
+    boolean isJavaFun = true;
+    boolean isFishTasty = false;
+    System.out.println(isJavaFun);
+    System.out.println(isFishTasty);
+  }
+}

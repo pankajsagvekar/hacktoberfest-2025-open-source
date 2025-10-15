e953d4d726219c5e5b49359b41475877b227535a
bimbims125 <pramvendread@gmail.com>
2025-10-15 08:46:51 +0700

    init: basic java

---
+++ basic_java/IntegerExample.java
@@ -0,0 +1,8 @@
+package basic_java;
+
+public class IntegerExample {
+    public static void main(String[] args) {
+        int number = 42;
+        System.out.println(number);
+    }
+}
---
+++ basic_java/StringExample.java
@@ -0,0 +1,7 @@
+package basic_java;
+public class StringExample {
+    public static void main(String[] args) {
+        String message = "Hello, World!";
+        System.out.println(message);
+    }
+}
